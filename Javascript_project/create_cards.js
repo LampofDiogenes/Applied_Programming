2697a35255acc3380ed8dd4d882547baa38e4010
Junky <wannabdiogens@gmail.com>
2023-11-02 22:25:35 -0600

    linked mechanics of card flipping to button

--- Javascript_project/create_cards.js
+++ Javascript_project/create_cards.js
@@ -1,5 +1,7 @@
-
+// setting up the list
 const card_image_list = [image1, image2, image3];
+
+
 // for each item in the list of card image, set the 
 // card_name to the current item in the list,
 // and then set up a div that 
@@ -12,5 +14,6 @@ for (i in card_image_list, i < len(card_image_list), i++)
     // setting the id of the div created as the name of the card
     div.className = card_name;
     // adding the div to the HTML file
+    <script src="card_flip.js"></script>
     document.appendChild(div);
 }
