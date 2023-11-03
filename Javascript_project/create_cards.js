fc5628f06a61edaf92ff626f4f733edbb8db260a
Junky <wannabdiogens@gmail.com>
2023-11-02 21:24:33 -0600

    added color to cards using CSS

--- Javascript_project/create_cards.js
+++ Javascript_project/create_cards.js
@@ -1,12 +1,12 @@
 
-const card_image_list = [];
+const card_image_list = [image1, image2, image3];
 // for each item in the list of card image, set the 
 // card_name to the current item in the list,
 // and then set up a div that 
 for (i in card_image_list, i < len(card_image_list), i++)
 {
     // setting the current card name from the list
-    const card_name = card_image_list[i];
+    let card_name = card_image_list[i];
     // creating a new div
     const div = document.createElement("div");
     // setting the id of the div created as the name of the card
